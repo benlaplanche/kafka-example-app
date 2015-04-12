8074ad63b6dcaf3da55737979a6074db17b67bb2
Ben Laplanche <ben@laplanche.co.uk>
2015-04-12 16:04:54 +0100

    working push to run.pivotal.io

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: kafka-example-app
--- main.go
+++ main.go
@@ -14,8 +14,8 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "80"
+		port = "8080"
 	}
-
+	fmt.Println("Port = " + port)
 	http.ListenAndServe(":"+port, api)
 }
